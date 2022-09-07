01acb80ff0c11828f578a9896f7c128230c4b720
mfernandez <mfernandez@ultimamillasa.com.ar>
2022-09-07 18:32:15 -0300

    post pokemon

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,10 @@
-export class CreatePokemonDto {}
+import { IsInt, IsPositive, IsString, MinLength } from "class-validator";
+
+export class CreatePokemonDto {
+  @IsInt()
+  @IsPositive()
+  readonly no: number;
+  @IsString()
+  @MinLength(1)
+  name: string;
+}
