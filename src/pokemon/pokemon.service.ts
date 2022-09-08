4f8ceffbcd9a2ddd6bb418295588dbcf402b8497
mfernandez <mfernandez@ultimamillasa.com.ar>
2022-09-08 17:21:52 -0300

    create pokemon

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { isValidObjectId, Model } from 'mongoose';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Pokemon } from './entities/pokemon.entity';
@@ -14,16 +14,34 @@ export class PokemonService {
   
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLowerCase();
-    const pokemon = await this.pokemonModel.create(createPokemonDto);
+    try {
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+      return pokemon;
+    } catch (error) {
+      if(error.code === 11000) {
+        throw new BadRequestException(`Pokemon exist in db ${JSON.stringify(error.keyValue)}`);
+      }      
+      console.log(error);
+      throw new InternalServerErrorException(`Can't created Pokemon - check server logs.`);
+    }
+  }
+
+  async findAll() {
+    const pokemons = await this.pokemonModel.find();
+    return pokemons;
+  }
+
+  async findOne(id: string) {
+    let pokemon: Pokemon;
+    if (!isNaN(+id))
+      pokemon = await this.pokemonModel.findOne({ no: id });
+    if (isValidObjectId(id))
+      pokemon = await this.pokemonModel.findById(id);
+    if (!pokemon)
+      pokemon = await this.pokemonModel.findOne({name: id.toLowerCase().trim() });
+    if (!pokemon)
+      throw new NotFoundException(`Resourse not found by no, id or name equal ${id}`);
     return pokemon;
-  }
-
-  findAll() {
-    return `This action returns all pokemon`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
   }
 
   update(id: number, updatePokemonDto: UpdatePokemonDto) {
